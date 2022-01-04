5aed14184a9cd81a14e1c1658c46bda404487726
Krzysztof Kosek <krzysiek.kosek@gmail.com>
2022-01-04 17:14:55 +0100

    Add image view

--- src/js/script.js
+++ src/js/script.js
@@ -112,6 +112,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -149,30 +150,22 @@
 
     processOrder(){
       const thisProduct = this;
-
-      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      //console.log('formData', formData);
-    
-      // set price to default price
+
       let price = thisProduct.data.price;
-      //console.log('price',price);
 
-      // for every category (param)...
       for(let paramId in thisProduct.data.params) {
-      // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
-        const param = thisProduct.data.params[paramId];
-        //console.log(paramId, param);
-         
-        
-        // for every option in this category
-        for(let optionId in param.options) {
-          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
-          const option = param.options[optionId];
-          console.log(optionId, option);
-          // check if there is param with a name of paramId in formData and if it includes optionId
 
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+        const param = thisProduct.data.params[paramId];    
+        
+        for(let optionId in param.options) {
+          
+          const option = param.options[optionId];
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          console.log(optionImage);
+
+          if(optionSelected) {
             // check if the option is not default
             if(!option.default == true) {
               price += option.price; 
@@ -181,6 +174,15 @@
             // check if the option is default
             if(option.default == true) {
               price -= option.price;
+            }
+          }
+
+          if (optionImage){
+            if (optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible); 
+            }
+            else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible); 
             }
           }
         }    
